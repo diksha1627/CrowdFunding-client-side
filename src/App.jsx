427c70a371d1c2b5ec960af119b49de54591d11a
Diksha <dikshabuxani84745@gmail.com>
2023-04-19 22:01:45 +0530

    feat:Routes created to home page navbar and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
-
+import Home  from './pages/Home';
+import { Sidebar , Navbar } from './components';
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#1313]">
-     
+
+      <div>
+        <Sidebar/>
+      </div>
+
+      <div>
+        <Navbar/>
+      </div>
+
+     <Routes>
+      <Route path="/" element={<Home />}/>
+     </Routes>
     </div>
   )
 }
---
+++ src/pages/CreateCampaign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateCampaign = () => {
+  return (
+    <div>
+      xxxx
+    </div>
+  )
+}
+
+export default CreateCampaign
