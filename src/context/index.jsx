85c4777787e8275ceae7733a42b956c6876851b0
Varun Doshi <doshivarun202@gmail.com>
2023-05-02 15:15:33 +0530

    UI update

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,73 +1,79 @@
-import React, { useContext, createContext } from 'react';
-import{ useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
-import { ethers } from 'ethers';
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
 
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-    //address of the smart contract
-    const { contract } = useContract('0x0a9F8aAFC21D0f2b570d38D6f4f50b591F41a3A7')
-    
-    //two ways to call write function
-    const  { mutateAsync: createCampaign } = 
-    useContractWrite(contract, 'createCampaign');//to call the function and create a campaign passing all the parameters
-    
-    
-    const  address  = useAddress();//address of smart wallet
-    const  connect = useMetamask();//to connect smart wallet
+  //address of the smart contract
+  const { contract } = useContract(
+    "0xd6E095D59F1B42225F0D6df5249B9EfaFb32dd10"
+  );
 
-    const publishCampaign = async(form)=>{
-        try {
+  //two ways to call write function
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  ); //to call the function and create a campaign passing all the parameters
 
-            const data  = await createCampaign([
-                address,
-                form.title,
-                form.description,
-                form.target,
-                new Date(form.deadline).getTime(),
-                form.image
-            ]);
+  const address = useAddress(); //address of smart wallet
+  const connect = useMetamask(); //to connect smart wallet
 
-            console.log('contract call success',data);
-            
-        } catch (error) {
-            console.log("contract call failure",data);
-        }
-   
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image,
+      ]);
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", data);
     }
+  };
 
-    const getCampaigns = async () =>{
-        const campaigns = await contract.call('getCampaigns');
-        console.log(campaigns);
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCompaigns");
+    console.log(campaigns);
 
-        //numbers into human readable format 
-        const parsedCampaigns = campaigns.map((campaign,i)=>({
-            owner: campaign.owner,
-            title: campaign.title,
-            description: campaign.description,
-            target: ethers.utils.formatEther(campaign.target.toString()),
-            deadline: deadline.toNumber(),
-            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
-            image:campaign.image,
-            pId:i,
-        }));
-        return (parsedCampaigns);
+    //numbers into human readable format
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+    return parsedCampaigns;
+  };
 
-    }
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
 
-    return (
-        <StateContext.Provider
-          value={{address,
-                 contract,
-                 connect,
-                 createCampaign : publishCampaign,
-                 getCampaigns
-            }}
-        >
-            {children}
-        </StateContext.Provider>
-    )
-}
-
-export const useStateContext = () => useContext (StateContext);
-
+export const useStateContext = () => useContext(StateContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-import { StateContextProvider } from './context';
-import App from './App';
-import './index.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
-        <Router>
-            <StateContextProvider>
-            <App /> 
-            </StateContextProvider>
-        </Router>
-    </ThirdwebProvider> 
-)
+  <ThirdwebProvider activeChain={ChainId.Mumbai}>
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
